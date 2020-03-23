fe1c64d6d19133d9de19cd1055da57fea8af9170
Vlad Popil <volodymyr.popil@gmail.com>
2020-03-23 01:45:07 -0400

    requirement.txt and unit test (simple)

--- webserver/pkgpkr/webservice/tests.py
+++ webserver/pkgpkr/webservice/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import AnonymousUser, User
 
 # Create your tests here.
+
+from .views import index
+
+
+class SimpleTest(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+
+    def test_details(self):
+        # Create an instance of a GET request.
+        request = self.factory.get("/")
+        request.user = AnonymousUser()
+
+        # Test my_view() as if it were deployed at /customer/details
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_vlad_bad(self):
+        # Create an instance of a GET request.
+        self.assertTrue(False)
