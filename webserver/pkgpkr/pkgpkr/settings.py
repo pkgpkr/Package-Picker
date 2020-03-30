ee897067f01b268dfed089e359360ce7ac7e7ce4
Daniel Sauble <djsauble@gmail.com>
2020-03-29 18:33:38 -0700

    Simplify ALLOWED_HOSTS list to just amazonaws.com domain

--- webserver/pkgpkr/pkgpkr/settings.py
+++ webserver/pkgpkr/pkgpkr/settings.py
@@ -29,9 +29,7 @@ ALLOWED_HOSTS = []
 
 # Add the container IP address as an allowed host if it exists
 if os.environ.get('ECS_CONTAINER_METADATA_URI'):
-  METADATA_URI = os.environ['ECS_CONTAINER_METADATA_URI']
-  container_metadata = requests.get(METADATA_URI).json()
-  ALLOWED_HOSTS.append(container_metadata['Networks'][0]['IPv4Addresses'][0])
+  ALLOWED_HOSTS.append('amazonaws.com')
 
 # Application definition
 
