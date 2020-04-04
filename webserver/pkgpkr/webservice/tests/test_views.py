d0e359cd5f9a2c7d82fb5725f85142012be3664d
Vlad Popil <volodymyr.popil@gmail.com>
2020-04-04 00:03:25 -0400

    added some api tests

--- webserver/pkgpkr/webservice/tests/test_views.py
+++ webserver/pkgpkr/webservice/tests/test_views.py
@@ -10,15 +10,37 @@ class SimpleTest(TestCase):
         # Every test needs access to the request factory.
         self.factory = RequestFactory()
 
-    def test_details(self):
+    def prep_request_and_call_method(self, path):
         # Create an instance of a GET request.
-        request = self.factory.get("/")
+        request = self.factory.get(path)
         request.user = AnonymousUser()
 
         # Test my_view() as if it were deployed at /customer/details
         response = index(request)
         self.assertEqual(response.status_code, 200)
 
-    def test_vlad_bad(self):
-        # TODO THIS IS DUMMY
-        self.assertTrue(True)
+
+    def test_index(self):
+        self.prep_request_and_call_method('/')
+
+    def test_about(self):
+        self.prep_request_and_call_method('/about')
+
+    def test_login(self):
+        self.prep_request_and_call_method('/login')
+
+
+    def test_callback(self):
+        self.prep_request_and_call_method('/callback')
+
+
+    def test_ilogout(self):
+        self.prep_request_and_call_method('/logout')
+
+
+    def test_repositories(self):
+        self.prep_request_and_call_method('/repositories')
+
+    def test_recommendations(self):
+         #, name
+        pass
