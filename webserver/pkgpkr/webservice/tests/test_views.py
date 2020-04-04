896497019954ec8bae9cdb4bcbd2e8bfef49360a
Vlad Popil <volodymyr.popil@gmail.com>
2020-04-04 16:19:24 -0400

    additional views test

--- webserver/pkgpkr/webservice/tests/test_views.py
+++ webserver/pkgpkr/webservice/tests/test_views.py
@@ -2,19 +2,38 @@
 from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import AnonymousUser, User
 
+from webservice import github_util
 from ..views import index, about, login, callback, logout, repositories
 
+import os
 
 class SimpleTest(TestCase):
     def setUp(self):
         # Every test needs access to the request factory.
         self.factory = RequestFactory()
 
-    def prep_request_and_call_method(self, path, method, exp_status_code=200):
+
+    def prep_not_githib_auth_request(self, path):
         # Create an instance of a GET request.
         request = self.factory.get(path)
         request.user = AnonymousUser()
         request.session = dict()
+        return request
+
+
+
+    def prep_with_githib_auth_request(self, path):
+        # Create an instance of a GET request.
+        request = self.factory.get(path)
+        request.user = AnonymousUser()
+        request.session = dict()
+        request.session['github_token'] = os.environ.get('TOKEN')
+        request.session['github_info'] = github_util.get_user_info(request.session['github_token'])
+        return request
+
+
+    def prep_request_and_call_method(self, request, method, exp_status_code=200):
+
 
         # Test my_view() as if it were deployed at /customer/details
         response = method(request)
@@ -23,13 +42,17 @@ class SimpleTest(TestCase):
 
 
     def test_index(self):
-        self.prep_request_and_call_method('/', index, )
+        request = self.prep_not_githib_auth_request('/')
+        self.prep_request_and_call_method(request, index)
 
     def test_about(self):
-        self.prep_request_and_call_method('/about', about)
+        request = self.prep_not_githib_auth_request('/about')
+        self.prep_request_and_call_method(request, about)
 
     def test_login(self):
-        self.prep_request_and_call_method('/login', login, 302)
+        request = self.prep_not_githib_auth_request('/login')
+        self.prep_request_and_call_method(request, login, 302)
+
         # TODO try with proper token should not redirect
 
     # TODO revisit after MS2
@@ -37,11 +60,18 @@ class SimpleTest(TestCase):
     #     self.prep_request_and_call_method('/callback', callback)
 
     def test_logout(self):
-        self.prep_request_and_call_method('/logout', logout, 302)
+        request = self.prep_not_githib_auth_request('/logout')
+        self.prep_request_and_call_method(request, logout, 302)
 
 
     def test_repositories(self):
-        self.prep_request_and_call_method('/repositories', repositories, 302)
+        request = self.prep_not_githib_auth_request('/repositories')
+        self.prep_request_and_call_method(request, repositories, 302)
+
+        request = self.prep_with_githib_auth_request('/repositories')
+        self.prep_request_and_call_method(request, repositories, 200)
+
+
         # TODO try with proper token should not redirect
 
 
