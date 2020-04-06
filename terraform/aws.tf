cc12dee977eb08cd9f3663ed41178a9f2aa4f948
Daniel Sauble <djsauble@gmail.com>
2020-04-05 22:18:47 -0700

    Get a new ECS cluster, service, load balancer, and vanilla nginx container running

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -3,6 +3,10 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+/**
+ * Set up daily trigger of the ML pipeline
+ */
+
 // Policy permitting someone to invoke a step function
 resource "aws_iam_role_policy" "invoke_step_function" {
   role = aws_iam_role.invoke_step_function.id
@@ -152,3 +156,100 @@ resource "aws_sfn_state_machine" "run_ml_pipeline" {
 }
 PATTERN
 }
+
+/**
+ * Set up ECS cluster, web service, and load balancer
+ */
+
+resource "aws_ecs_cluster" "pkgpkr" {
+  name = "pkgpkr"
+}
+
+resource "aws_ecs_service" "web" {
+  name = "pkgpkr-web"
+  cluster = aws_ecs_cluster.pkgpkr.id
+  task_definition = aws_ecs_task_definition.web.arn
+  desired_count = 1
+  launch_type = "FARGATE"
+  
+  network_configuration {
+    subnets = [
+      "subnet-026cbcabb97c47674",
+      "subnet-0f693ef4738a1ecb3"
+    ]
+    security_groups = [
+      "sg-0d4030427b648e2bd"
+    ]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.web.arn
+    container_name = "nginx"
+    container_port = 80
+  }
+
+  depends_on = [
+    aws_lb_listener.web
+  ]
+}
+
+resource "aws_ecs_task_definition" "web" {
+  execution_role_arn = "arn:aws:iam::392133285793:role/ecsTaskExecutionRole"
+  container_definitions = file("task-definitions/pkgpkr-web-service.json")
+  memory = "512"
+  family = "pkgpkr-web-task-definition"
+  requires_compatibilities = [
+    "FARGATE"
+  ]
+  network_mode = "awsvpc"
+  cpu = "256"
+}
+
+resource "aws_lb" "web" {
+  subnets = [
+    "subnet-026cbcabb97c47674",
+    "subnet-0f693ef4738a1ecb3"
+  ]
+  security_groups = [
+    "sg-0e12259ab9ddfa5dd"
+  ]
+}
+
+resource "aws_lb_target_group" "web" {
+  target_type = "ip"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = "vpc-033ea67095da124a4"
+}
+
+resource "aws_lb_listener" "web" {
+  load_balancer_arn = aws_lb.web.id
+  port = "80"
+  protocol = "HTTP"
+
+  // TODO: Set up SSL and a separate listener on 443, then redirect this to 443
+  /*default_action {
+    type = "redirect"
+
+    redirect {
+      port = "443"
+      protocol = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }*/
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.web.arn
+  }
+}
+
+resource "aws_cloudwatch_log_group" "web" {
+  name = "/ecs/pkgpkr-web"
+  retention_in_days = 30
+}
+
+resource "aws_cloudwatch_log_stream" "web" {
+  name = "pkgpkr-web"
+  log_group_name = aws_cloudwatch_log_group.web.name
+}
+
