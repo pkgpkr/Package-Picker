12a963ce0ad0b3b234786fdcaa0b3b25ffb0e0aa
Daniel Sauble <djsauble@gmail.com>
2020-04-07 23:20:19 -0700

    Move client.py to scraper folder, adjust GitHub action to trigger on push only

--- pipeline/scraper/client.py
+++ pipeline/scraper/client.py
@@ -2,11 +2,10 @@
 Spin up a new database for testing and run scraper tests
 """
 
+import test
 import os
 import sys
 import psycopg2
-sys.path.append('./pipeline/scraper')
-import test
 
 try:
     USER = 'postgres'
