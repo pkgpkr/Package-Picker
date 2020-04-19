3272f8f548ea6aed3400f810389fdea75754be2d
Daniel Sauble <djsauble@gmail.com>
2020-04-18 17:29:07 -0700

    Switch from TOKEN to GITHUB_TOKEN

--- webserver/pkgpkr/webservice/tests/test_github_utils.py
+++ webserver/pkgpkr/webservice/tests/test_github_utils.py
@@ -22,7 +22,7 @@ class TestGithubUtil(TestCase):
     """
 
     def setUp(self):
-        self.github_token = os.environ.get('TOKEN')
+        self.github_token = os.environ.get('GITHUB_TOKEN')
 
     def test_get_user_info(self):
         """
