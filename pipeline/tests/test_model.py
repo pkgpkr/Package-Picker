a1da01c6adefa92d1f216a90473b4f021e1e8637
Daniel Sauble <djsauble@gmail.com>
2020-04-25 15:56:53 -0700

    Add basic setup and tear down steps for the model tests

--- pipeline/tests/test_model.py
+++ pipeline/tests/test_model.py
@@ -2,7 +2,9 @@
 Tests for the pipeline model
 """
 
+import os
 import unittest
+import psycopg2
 
 class TestScraper(unittest.TestCase):
     """
@@ -10,10 +12,25 @@ class TestScraper(unittest.TestCase):
     """
 
     def setUp(self):
-        pass
+
+        # Connect to the database
+        USER = os.environ.get('DB_USER')
+        PASSWORD = os.environ.get('DB_PASSWORD')
+        HOST = os.environ.get('DB_HOST')
+        DATABASE = os.environ.get('DB_DATABASE')
+        PORT = os.environ.get('DB_PORT')
+        CONN_STRING = f"host={HOST} user={USER} password={PASSWORD} dbname={DATABASE} port={PORT}"
+        self.database = psycopg2.connect(CONN_STRING)
+        self.cursor = self.database.cursor()
+
+        # TBD: Populate with data
 
     def tearDown(self):
-        pass
+
+        # TBD: Clean out all data
+        
+        self.cursor.close()
+        self.database.close()
 
     def test_update_bounded_similarity_scores(self):
         pass
