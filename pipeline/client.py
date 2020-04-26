e80e45bb2c660f2711d923e95cbe62a181c19562
Daniel Sauble <djsauble@gmail.com>
2020-04-26 11:10:37 -0700

    Move database provisioning to separate script

--- pipeline/client.py
+++ pipeline/client.py
@@ -18,51 +18,8 @@ try:
                                   host=HOST,
                                   port=5432,
                                   database=DATABASE)
-    CURSOR = CONNECTION.cursor()
-    INIT_TABLES = """
-CREATE TABLE applications (
-  id SERIAL PRIMARY KEY,
-  url TEXT NOT NULL,
-  name TEXT NOT NULL,
-  followers INTEGER,
-  hash TEXT NOT NULL,
-  retrieved TIMESTAMPTZ NOT NULL,
-  CONSTRAINT unique_url UNIQUE (url)
-);
-CREATE TABLE packages (
-  id SERIAL PRIMARY KEY,
-  name TEXT UNIQUE NOT NULL,
-  short_name TEXT,
-  url TEXT,
-  monthly_downloads_last_month INTEGER,
-  monthly_downloads_a_year_ago INTEGER,
-  absolute_trend INTEGER,
-  relative_trend INTEGER,
-  categories TEXT[],
-  popularity INTEGER,
-  bounded_popularity INTEGER,
-  modified TIMESTAMPTZ,
-  display_date TEXT,
-  retrieved TIMESTAMPTZ NOT NULL
-);
-CREATE TABLE dependencies (
-  application_id INTEGER REFERENCES applications (id),
-  package_id INTEGER REFERENCES packages (id),
-  CONSTRAINT unique_app_to_pkg UNIQUE (application_id, package_id)
-);
-CREATE TABLE similarity (
-  package_a INTEGER REFERENCES packages (id),
-  package_b INTEGER REFERENCES packages (id),
-  similarity FLOAT(4) NOT NULL,
-  bounded_similarity INTEGER,
-  CONSTRAINT unique_pkg_to_pkg UNIQUE (package_a, package_b)
-);
-CREATE INDEX ON similarity (package_a);
-CREATE INDEX ON similarity (package_b);
-CREATE INDEX ON similarity (similarity);
-    """
-    CURSOR.execute(INIT_TABLES)
-    CONNECTION.commit()
+    with CONNECTION.cursor() as cursor:
+      cursor.execute(open("provision_db.sql", "r").read())
     RESULT = os.system(f"""
 DB_USER={USER} \
 DB_PASSWORD={PASSWORD} \
@@ -76,7 +33,6 @@ except psycopg2.Error as error:
 finally:
     #closing database connection.
     if CONNECTION:
-        CURSOR.close()
         CONNECTION.close()
         print("PostgreSQL connection is closed")
     if RESULT is None:
--- pipeline/scraper/pypi.py
+++ pipeline/scraper/pypi.py
@@ -29,11 +29,11 @@ def run_query():
         print(f"Fetching metadata for {result}")
         metadata = get_package_metadata(result)
         update_package_metadata(database,
-                                metadata['name'],
-                                metadata['monthly_downloads_last_month'],
-                                metadata['monthly_downloads_a_year_ago'],
-                                metadata['categories'],
-                                metadata['modified'])
+                                metadata.get('name'),
+                                metadata.get('monthly_downloads_last_month'),
+                                metadata.get('monthly_downloads_a_year_ago'),
+                                metadata.get('categories'),
+                                metadata.get('modified'))
 
         # Commit the changes to the database
         database.commit()
